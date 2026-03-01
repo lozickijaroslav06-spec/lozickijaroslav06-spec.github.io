fdd73e499e7e8fd621c08f88a9bab568f5fbc419
lozickijaroslav06-spec <lozickijaroslav06@gmail.com>
2026-03-01 18:13:50 +0200

    Add files via upload

--- sync.js
+++ sync.js
@@ -1,14 +1,2 @@
-import { handleBackgroundOptionFor } from '../../../static/js/customizer/sync/variables/background'
-import ctEvents from 'ct-events'
-
-import { getRootSelectorFor } from '../../../static/js/customizer/sync/helpers'
-
-/*
-ctEvents.on(
-	'ct:header:sync:collect-variable-descriptors',
-	(variableDescriptors) => {
-	}
-)
-
-ctEvents.on('ct:header:sync:item:global', ({ optionId, optionValue }) => {})
-*/
+import '../../framework/features/header/items/account/sync'
+import './sync/header'
